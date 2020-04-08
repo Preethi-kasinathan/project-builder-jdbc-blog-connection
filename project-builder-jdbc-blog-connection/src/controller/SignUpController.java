0ae90933d32493334839886e1b4b2c256ceeeab0
Preethi-kasinathan <61906137+Preethi-kasinathan@users.noreply.github.com>
2020-04-08 18:03:01 +0530

    Add files via upload

---
+++ project-builder-jdbc-blog-connection/src/controller/SignUpController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import utility.ConnectionManager;
+
+
+
+@WebServlet(urlPatterns= {"/signup"})
+public class SignUpController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public SignUpController() {
+        super();
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Sign up Controller");
+		
+		ConnectionManager cm = new ConnectionManager();
+		
+		try {
+		Connection con =cm.getconnection();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
+		rd.forward(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request,response);
+		
+	}
+
+}
